a4a5e6c003e8a68daa5c31fa9ed35b24e8524bf3
god0108 <god0108p@gmail.com>
2024-12-24 18:53:04 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: '안녕하세요',
+    introDesc: `Xin chào người xinh đẹp nhất tớ biết`,
+    btnIntro: 'hihi',
+    title: 'Merry Christmas !!! 🥰🎄🎄',
+    desc: 'Bạn có phải là người xinh đẹp nhất không zậy ??? ',
+    btnYes: 'Chắc chắn là như vậy rồi chứ gì nữa',
+    btnNo: 'Hơi hơi thôi🤔',
+    question:'Cậu có muốn gửi gì đến tớ không ??? ',
+    btnReply: 'Gửi',
+    reply: 'Muốn làm bạn với cậu mãi mãi dù có giận thì chỉ giận xíu xíu thôi :>>>',
+    mess: 'Ahihi cảm ơn cậu nhó tớ cũm zậy',
+    messDesc: 'Đây là phần quà đặc biệt của tớ nè',
+    btnAccept: 'Okiiiii lun',
+    messLink: 'https://vt.tiktok.com/ZS6jqgPAu/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
